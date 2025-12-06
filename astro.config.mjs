bb5755bb3b20784f9076aef48137e5dff7cc2735
Maikpwwq <mariasf@correo.udistrital.edu.co>
2025-12-06 10:55:44 -0500

    feat: Initial commit Katherin Nails website

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
+
+import db from '@astrojs/db';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [db()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
